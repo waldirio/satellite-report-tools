83d5a9a9ba1f254679b7b8698df97830a1bfcea8
Waldirio <waldirio@gmail.com>
2013-05-23 14:26:23 -0300

    add function to select the input file (list with cve/rhsa) and select the output file name.

--- SatelliteReportTools.py
+++ SatelliteReportTools.py
@@ -1,4 +1,3 @@
-
 # 
 # Date ........: 05/22/2013
 # Developer ...: Waldirio M. Pinheiro <waldirio.pinheiro@iesbrazil.com.br>
@@ -13,58 +12,56 @@ from macpath import split
 
 # Global Vars
 global list
+nameFileOut=""
+nameFileIn=""
+
+def defineInPutFile():
+    global nameFileIn
+    nameFileIn = raw_input('Inform the list file name with cve/rhsa: ')
+    
+
+def defineOutPutFile():
+    global nameFileOut
+    nameFileOut = raw_input('inform the output file name: ')
+    fileRef = open(nameFileOut,'w')
+    fileRef.write('NODE;CVE;RHSA\n')
+    fileRef.close()
+
+def dumpOutFile(params_nodes):
+    fileRef = open(nameFileOut,'a')
+    fileRef.write(params_nodes)
+    fileRef.close()
 
 def findSystemCve(cveCode):
-#    print cveCode
-    
     ErrataFindCve = config.client.errata.findByCve(config.key,cveCode)
-    
     AdvCode=ErrataFindCve[0].get('advisory_name')
-    print AdvCode
     
     for b in [AdvCode]:
         ErrataAffectedSystems = config.client.errata.listAffectedSystems(config.key,b)
-#        print ErrataAffectedSystems[0].get('system_name')
-        print len(ErrataAffectedSystems)
-        
+       
         for x in range(len(ErrataAffectedSystems)):
-            print ErrataAffectedSystems[x].get('system_name')
+            dumpOutFile(ErrataAffectedSystems[x].get('system_name') + ";" + cveCode + ";" + "-" + ";" +"\n")
         
-#$PreCommand errata.findByCve "%session%" $CodCve                                                      > $LOG
-#AdvCode=$(grep advisory_name $LOG |awk -F"=>" '{print $2}'|sed -e "s/'//g"|sed -e "s/,//g"|sed -e "s/^ //g")
-
-#for b in $AdvCode
-#do
-#  echo "CVE Cod. $b"
-#  $PreCommand errata.listAffectedSystems "%session%" $b                                               > $LOG
-#  grep system_name $LOG|awk -F"=>" '{print $2}'|sed -e "s/'//g"|sed -e "s/,//g"|sed -e "s/^ //g"      > $LOG1
-
-#  while read line
-#  do
-#    echo "$line;$CodCve;$b;$(date +%d/%m/%Y)"                                >> $LOGDIR/$LOGNAMEFILE
-#  done < $LOG1
-
-#done
-    
-    
-    
 
 def findSystemAdv(advCode):
-    print advCode
-    
-    
+    ErrataFindAdv = config.client.errata.listAffectedSystems(config.key,advCode)
+    for x in range(len(ErrataFindAdv)):
+        dumpOutFile(ErrataFindAdv[x].get('system_name') + ";" + "-" + ";" + advCode + ";" + "\n")
     
 def listCveRhsa():
-    file=open('list.txt', 'r')
+    fileRef=open(nameFileIn, 'r')
     
-    for b in file:
+    for b in fileRef:
     	typeAdv=b[0:3]
     	if typeAdv == 'RHS':
-            print 'Security Advisory - ' + b
-            findSystemAdv(b)
+#            print 'Security Advisory - ' + b
+            # Add .rstrip to remove '\n' char
+            findSystemAdv(b.rstrip())
     	elif typeAdv == 'CVE':
-            print 'CVEs - ' + b
+#            print 'CVEs - ' + b
             findSystemCve(b)
+        else:
+            print 'discarding ...., comments or blank line'
 
 def listUsers():
     for user in config.list:
@@ -75,7 +72,9 @@ def menu():
 #        os.system('cls')
         print '##################################'
         print '# 0. List users from RHNSatellite'
-        print '# 1. Check CVE/RHSA from file'
+        print '# 1. Define the output file name'
+        print '# 2. Define the CVEs/RHSA file name'
+        print '# 3. Check CVE/RHSA from file'
         print '#'
         print '# 9. Exit'
         print '##################################'
@@ -85,6 +84,10 @@ def menu():
         if opc == '0':
             listUsers()
         elif opc == '1':
+            defineOutPutFile()
+        elif opc == '2':
+            defineInPutFile()
+        elif opc == '3':
             listCveRhsa()
         elif opc == '9':
             print 'Sair ...'
